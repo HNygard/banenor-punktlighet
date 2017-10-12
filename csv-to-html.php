e4775f34eabf70388a8d05db14f3471e58c11fa5
Hallvard Nygard <hallvard.nygard@gmail.com>
2017-10-13 00:58:07 +0200

    Utlisting av avganger/ankomster med planlagt tid, faktisk tid og diff. Simple styling og info tekst.

--- csv-to-html.php
+++ csv-to-html.php
@@ -5,6 +5,9 @@ $datasettBeskrivelse = 'Togtrafikk på Stavanger Stasjon i perioden 01.01.2017 t
 
 $csvHeadings = explode(',', trim($csvLines[0]));
 var_dump($csvHeadings);
+for($i = 0; $i < count($csvHeadings); $i++) {
+	$csvHeadings[$i] = str_replace(' ', '_', $csvHeadings[$i]);
+}
 
 $alleAvganger = array();
 $alleAnkomster = array();
@@ -56,6 +59,11 @@ for($i = 1; $i < count($csvLines); $i++) {
 	unset($obj->stasjonsbruk);
 	unset($obj->linjenummer);
 
+	// Parse tid
+	$obj->planlagt_ankomst_unixtime = norskTidTilUnixtime($obj->planlagt_ankomst);
+	$obj->faktisk_ankomst_unixtime = norskTidTilUnixtime($obj->faktisk_ankomst);
+	$obj->planlagt_avgang_unixtime = norskTidTilUnixtime($obj->planlagt_avgang);
+	$obj->faktisk_avgang_unixtime = norskTidTilUnixtime($obj->faktisk_avgang);
 }
 
 // Sjekk at alle med samme tognummer har samme avgangstid, ankomststed, etc
@@ -93,20 +101,138 @@ var_dump($alleAnkomster[0]);
 ksort($perTognrAvganger);
 ksort($perTognrAnkomster);
 
+function norskTidTilUnixtime($norskTid) {
+	if ($norskTid == '_') {
+		return -1;
+	}
+	// 01.01.2017 00:00
+	$unixtime = mktime(
+		// Hour
+		substr($norskTid, 11, 2),
+		// Minute
+		substr($norskTid, 14, 2),
+		// Second
+		0,
+		// Month
+		substr($norskTid, 3, 2),
+		// Day of month
+		substr($norskTid, 0, 2),
+		// Year
+		substr($norskTid, 6, 4)
+	);
+	if (date('d.m.Y H:i', $unixtime) != $norskTid
+		// Noe spesielt med denne dagen og tidspunktet. Stilte vi klokka?
+		&& $norskTid != '26.03.2017 02:17'
+		&& $norskTid != '26.03.2017 02:18') {
+		throw new Exception('Error parsing date: ' . chr(10) . $norskTid . chr(10) . date('d.m.Y H:i', $unixtime));
+	}
+	return $unixtime;
+}
 function tognrLink ($tognrOgAvgang) {
 	return 'tognr-'.str_replace(' ', '-', str_replace(':', '', $tognrOgAvgang)) . '.html';
 }
+function getDiffTekst($planlagt, $faktisk, $innstiltTog, $delinnstiltStv) {
+	if ($innstiltTog == 'Y') {
+		$innstiltTekst = 'Helinnstilt tog';
+	}
+	else if ($innstiltTog == 'P') {
+		$innstiltTekst = 'Delinnstil tog';
+	}
+	else if ($innstiltTog == 'B') {
+		$innstiltTekst = 'Buss for tog';
+	}
+	else if ($innstiltTog == 'N') {
+		$innstiltTekst = '';
+	}
+	else {
+		throw new Exception('Ukjent innstilt tog verdi: ' . $innstiltTog);
+	}
+	return ($faktisk == -1 ? '?' : (($faktisk - $planlagt) / 60)) . ' minutter'
+		. '. ' . $innstiltTekst;
+}
+
+$simpleStyling = '<style>
+table {
+    border-collapse: collapse;
+}
+table td, table th {
+	border: 1px solid black;
+}
+</style>
+<span style="font-size: 0.8em;">Tograpport generert av <a href="https://twitter.com/hallny">@hallny</a> (Hallvard Nygård)
+ - <a href="https://github.com/HNygard/banenor-punktlighet">Kildekode på Github.</a><br><br></span>
+';
+
 function writeAvgangsliste($fil, $tittel, $avganger) {
+	global $simpleStyling;
 	$content = '<h1>' . $tittel . '</h1>' . chr(10);
+	$content .= $simpleStyling;
 	$content .= 'Antall avganger: ' . count($avganger) . chr(10);
+
+	$content .= '<table>' . chr(10);
+	$content .= '<thead><tr>' . chr(10);
+	$content .= '<th>Togtype</th>' . chr(10);
+	$content .= '<th>Planlagt avgang</th>' . chr(10);
+	$content .= '<th>Faktisk avgang</th>' . chr(10);
+	$content .= '<th>Differanse</th>' . chr(10);
+	$content .= '</tr></thead>' . chr(10);
+
+	$content .= '<tbody>' . chr(10);
+	foreach($avganger as $avgang) {
+		$content .= '<tr>' . chr(10);
+		$content .= '<td>' . $avgang->togtype_nv . '</td>' . chr(10);
+		$content .= '<td>' . $avgang->planlagt_avgang . '</td>' . chr(10);
+		$content .= '<td>' . $avgang->faktisk_avgang . '</td>' . chr(10);
+		$content .= '<td>' . getDiffTekst(
+				$avgang->planlagt_avgang_unixtime,
+				$avgang->faktisk_avgang_unixtime,
+				$avgang->innstilt_tog,
+				$avgang->delinnstilt_STV
+			) . '</td>' . chr(10);
+		$content .= '</tr>' . chr(10);
+	}
+	$content .= '</tbody>' . chr(10);
+
+	$content .= '</table>' . chr(10);
+
 	file_put_contents($fil, $content);
 }
 function writeAnkomstliste($fil, $tittel, $avganger) {
+	global $simpleStyling;
 	$content = '<h1>' . $tittel . '</h1>' . chr(10);
-	$content .= 'Antall ankomster: ' . count($avganger) . chr(10);
+	$content .= $simpleStyling;
+	$content .= 'Antall ankomster: ' . count($avganger) . chr(10) . chr(10);
+
+	$content .= '<table>' . chr(10);
+	$content .= '<thead><tr>' . chr(10);
+	$content .= '<th>Togtype</th>' . chr(10);
+	$content .= '<th>Planlagt ankomst</th>' . chr(10);
+	$content .= '<th>Faktisk ankomst</th>' . chr(10);
+	$content .= '<th>Differanse</th>' . chr(10);
+	$content .= '</tr></thead>' . chr(10);
+
+	$content .= '<tbody>' . chr(10);
+	foreach($avganger as $avgang) {
+		$content .= '<tr>' . chr(10);
+		$content .= '<td>' . $avgang->togtype_nv . '</td>' . chr(10);
+		$content .= '<td>' . $avgang->planlagt_ankomst . '</td>' . chr(10);
+		$content .= '<td>' . $avgang->faktisk_ankomst . '</td>' . chr(10);
+		$content .= '<td>' . getDiffTekst(
+				$avgang->planlagt_ankomst_unixtime,
+				$avgang->faktisk_ankomst_unixtime,
+				$avgang->innstilt_tog,
+				$avgang->delinnstilt_STV
+			) . '</td>' . chr(10);
+		$content .= '</tr>' . chr(10);
+	}
+	$content .= '</tbody>' . chr(10);
+
+	$content .= '</table>' . chr(10);
+
 	file_put_contents($fil, $content);
 }
 $content = '<h1>' . $datasettBeskrivelse . '</h1>' . chr(10);
+$content .= $simpleStyling;
 $content .= '<table class="table" style="width: 100%;"><tr><td>' . chr(10) . '<h2>Avganger</h2>' . chr(10);
 foreach($perTognrAvganger as $tognrOgAvgang => $avganger) {
 	$content .= '<li><a href="' . tognrLink($tognrOgAvgang) . '">' . $tognrOgAvgang . '</a> - ' . count($avganger) . ' avganger' . chr(10);
